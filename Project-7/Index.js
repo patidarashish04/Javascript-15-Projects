f581af0aa1df087a48333755a3a32fbedbd7cf2e
patidarashish04 <patidarashish04@gmail.com>
2021-12-29 18:38:30 +0530

    Update7thproject

---
+++ Project-7/Index.js
@@ -0,0 +1,26 @@
+const questions = document.querySelectorAll(".question")
+
+questions.forEach((question)=>{
+    const btns = document.querySelector(".question-btn")
+btns.addEventListener("click", ()=>{
+questions.forEach((item)=>{
+    if(item !== question){
+        item.classList.remove("show-text")
+    }
+});
+        question.classList.toggle("show-text")  
+    
+});
+});
+
+
+
+//teversing the DOM
+// const btns = document.querySelectorAll(".question-btn")
+
+// btns.forEach(function(btns){
+//     btns.addEventListener("click", function(e){
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle("show-text")
+// })
+// })
